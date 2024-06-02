c92d264902ea5d72e4e421ee6b3cf97fd2d90b7b
Zweli23 <zwelihle62@gmail.com>
2024-06-02 16:03:14 +0200

    Added files

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import handlebars from 'vite-plugin-handlebars';
+import data from './data.json';
+
+export default {
+  plugins: [
+    handlebars({
+      context: data // replace the curly brackets with data.json
+    }),
+  ],
+};
